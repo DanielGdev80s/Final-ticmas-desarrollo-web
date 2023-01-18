4ea966385680ac724f16e077a48617613a9c5145
unknown <danielgomez1668@hotmail.com>
2023-01-17 23:53:18 -0300

    tp final desarrollo web

---
+++ CVjs.js
@@ -0,0 +1,20 @@
+
+document.getElementById('twiter').addEventListener('click', () => {
+    window.open('https://twitter.com/TheHorrorMaster')
+   });
+
+   document.getElementById('facebook').addEventListener('click', () => {
+    window.open("https://www.facebook.com/directorjohncarpenter/")
+   });
+
+   document.getElementById('instagram').addEventListener('click', () => {
+    window.open("https://www.instagram.com/JohnCarpenterOfficial/")
+   });
+
+   document.getElementById('youtube').addEventListener('click', () => {
+    window.open("https://www.youtube.com/channel/UCL8qXl5HI0v_XxGN8CFQxLw")
+   });
+
+   document.getElementById('web').addEventListener('click', () => {
+    window.open("https://theofficialjohncarpenter.com/")
+   });
